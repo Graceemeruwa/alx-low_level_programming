a8b5473388231e0c944aaf86ab6db49d225c3305
Grace emeruwa <jgrace502@gmail.com>
2022-09-13 15:59:35 -0700

    0-putcher

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main: entry entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{	char *sh = "putcher";
+
+		while (*sh)
+
+		{
+			_putchar(*sh);
+			sh++;	
+		}
+	
+	_putchar('\n');
+
+	return (0)
+		
+}

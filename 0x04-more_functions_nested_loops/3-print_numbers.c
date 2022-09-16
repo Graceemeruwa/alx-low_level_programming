5e87ae9da5fdb068f8ccecc28cf7ca6c6da5ae60
Grace emeruwa <jgrace502@gmail.com>
2022-09-15 17:58:33 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+
+
+/**
+ * print_numbers - a function that  prints the numbers from  0 to 9
+ *             only using _putchar twice
+ *
+ * Return: Always 0-9 followed by newline
+ */
+
+void print_numbers(void)
+
+{
+int i;
+	for (i = 0, i <= 9; i++;)
+		_putchar(i + '0');
+
+	_putchar('\n');
+
+}

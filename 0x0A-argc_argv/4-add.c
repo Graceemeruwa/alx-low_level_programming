62ae6edc9f915bbc24918e87f3fefa766db82a3c
Grace emeruwa <jgrace502@gmail.com>
2022-10-03 08:22:39 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 #include <stdlib.h>
@@ -17,53 +16,20 @@
  */
 
 int main(int argc, char *argv[])
-
 {
-
-	int i, j, sum = 0;
-
-
-
-	if (argc == 1)
-
-	{
-
-		printf("0\n");
-
-	}
-
-	else
-
-	{
-
-		for (i = 1; i < argc; i++)
-
-		{
-
-			for (j = 0; argv[i][j] != 0; j++)
-
-			{
-
-				if (argv[i][j] > 58 || argv[i][j] < 47)
-
-				{
-
-					printf("Error\n");
-
-					return (1);
-
-				}
-
-			}
-
-			sum = sum + atoi(argv[i]);
-
-		}
-
-		printf("%d\n", sum);
-
-	}
-
-	return (0);
-
+int num, digit, sum = 0;
+for (num = 1; num < argc; num++)
+{
+for (digit = 0; argv[num][digit]; digit++)
+{
+if (argv[num][digit] < "0" || argv[num][digit] > "9")
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[num]);
+}
+printf("%d\n", sum);
+return (0);
 }
